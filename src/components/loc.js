47d91fb416a12fbc98022955dd605ec575741fa2
Lars <39798840+LarsWalraevens@users.noreply.github.com>
2021-09-17 17:39:38 +0200

    Cleanup & small tweaks

--- src/components/loc.js
+++ src/components/loc.js
@@ -1,17 +1,17 @@
-import React, { Component, useState,useEffect, useRef, ReactDOM } from 'react';
-import {MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React from 'react';
+import {MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 // Ant
-import { Input, AutoComplete, List } from 'antd';
+import { Input, AutoComplete, List, Col, Row } from 'antd';
 import * as L from "leaflet";
 import 'antd/dist/antd.css'
 const { Search } = Input;
 
+// Markers fix
 delete L.Icon.Default.prototype._getIconUrl;
-
 let DefaultIcon = L.icon({
     iconUrl: icon,
     shadowUrl: iconShadow
@@ -19,13 +19,13 @@ let DefaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
+
 const Loc = (props) => {    
     const [map, setMap] = React.useState();
     const defaultCenter = [50.6402809, 4.6667145];
     const defaultZoom= 10;
     let suggestions = [];
     const resultList = document.getElementById('result-list');
-    const list = document.getElementById('list');
     const currentMarkers = [];   
 
     const onSearch = (searchText) => {
@@ -35,32 +35,31 @@ const Loc = (props) => {
                 suggestions= parsedResult;
                 SetResultList(parsedResult);
             }); 
+            resultList.innerHTML = "<h2>List of results</h2> <div>Options for: '" + searchText + "'</div>";;
     };
     
-    function SetResultList(parsedResult) {        
-        let arrayList = [];
-
-        if(resultList!== null) {
-            resultList.innerHTML = "<h2>List of results</h2>";            
-        }
+    function SetResultList(parsedResult) {  
 
         for (const marker of currentMarkers) {
             map.removeLayer(marker);
         }
         
-        map.flyTo(new L.LatLng(20.13847, 1.40625), 2);
+        // Reset to full map (not that fast)
+        // map.flyTo(new L.LatLng(20.13847, 1.40625), 2);
+
         for (const result of parsedResult) {
+
+            // Create li for every suggestion
             const li = document.createElement('li');
             li.classList.add('list-group-item', 'list-group-item-action');
             li.innerHTML = result.display_name;
             li.setAttribute("data", JSON.stringify({
-                    displayName: result.display_name,
-                    lat: result.lat,
-                    lon: result.lon
-                }, undefined, 2));
+                displayName: result.display_name,
+                lat: result.lat,
+                lon: result.lon
+            }, undefined, 2));
 
-            arrayList.push(result.display_name);
-
+            // Add click event
             li.addEventListener('click', (event) => {
                 for(const child of resultList.children) {
                     child.classList.remove('active');
@@ -69,9 +68,7 @@ const Loc = (props) => {
                 const clickedData = JSON.parse(event.target.getAttribute("data"));
                 const position = new L.LatLng(clickedData.lat, clickedData.lon);
                 map.setView(position, 17);
-            })
-            // list.setAttribute("dataSource", [arrayList]);
-            // console.log(arrayList);
+            });
             const position = new L.LatLng(result.lat, result.lon);
             currentMarkers.push(new L.marker(position).addTo(map));
             resultList.appendChild(li);
@@ -80,31 +77,25 @@ const Loc = (props) => {
   
     return (
         <div id='loc' style={{marginTop:"100px"}}>
-            <h1>Look up a place</h1>
-            <div>
-                <Search placeholder="Street, adress, city, ..." onSearch={onSearch} style={{ width: 400 }} />  
-            </div>
-            <div className="map-result">
-                <div id="result-list">
+            <Row className="map-result">
+                <Col span={8} id="result-list">
                     <h2>List of results</h2>
-                    {/* <List id="list" size="small" bordered renderItem={item => <List.Item>{item}</List.Item>} /> */}
-                    {/* <List
-      size="small"
-      bordered
-      dataSource={["something"]}
-      renderItem={item => <List.Item>{item}</List.Item>}
-    /> */}
-                </div>
-                <div id="result-map" style={{height:"80vh"}}>
-                <MapContainer whenCreated={setMap} center={defaultCenter} zoom={defaultZoom} scrollWheelZoom={true}>
-                    <TileLayer
-                    attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                    url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
-                    />
-
-                </MapContainer>
-                </div>
-            </div>
+                </Col>
+                <Col span={16} id="result-map" style={{height:"80vh"}}>
+                    <div style={{textAlign:"left", marginBottom:"40px"}}>
+                        <h1>Look up a place</h1>
+                        <div>
+                            <Search placeholder="Street, adress, city, ..." onSearch={onSearch} style={{ width: 400 }} />  
+                        </div>
+                    </div>
+                    <MapContainer whenCreated={setMap} center={defaultCenter} zoom={defaultZoom} scrollWheelZoom={true}>
+                        <TileLayer
+                        attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                        url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+                        />
+                    </MapContainer>
+                </Col>
+            </Row>
         </div>
     );
 }
